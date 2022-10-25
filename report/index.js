2afc586a333beb7b36905ade9737a052f549045b
Maxwell O'Roark <m.d.oroark@gmail.com>
2022-10-24 20:55:51 -0500

    have buffer

--- report/index.js
+++ report/index.js
@@ -1,3 +1,6 @@
+const { Storage } = require("@google-cloud/storage");
+const storage = new Storage();
+
 // [START functions_helloworld_storage]
 /**
  * Generic background Cloud Function to be triggered by Cloud Storage.
@@ -6,7 +9,7 @@
  * @param {object} file The Cloud Storage file metadata.
  * @param {object} context The event metadata.
  */
-exports.generateReport = (file, context) => {
+exports.generateReport = async (file, context) => {
   console.log(`  Event: ${context.eventId}`);
   console.log(`  Event Type: ${context.eventType}`);
   console.log(`  Bucket: ${file.bucket}`);
@@ -14,5 +17,7 @@ exports.generateReport = (file, context) => {
   console.log(`  Metageneration: ${file.metageneration}`);
   console.log(`  Created: ${file.timeCreated}`);
   console.log(`  Updated: ${file.updated}`);
+  const data = await storage.bucket(file.bucket).file(file.name).download(); //stream is created
+  console.log(data[0]);
 };
 // [END functions_helloworld_storage]
