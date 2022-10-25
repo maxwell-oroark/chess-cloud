4bbdda445bdc6a73604b7331567624c78ff24f36
maxwell oroark <m.d.oroark@gmail.com>
2022-10-24 21:46:47 -0500

    with reports

--- report/index.js
+++ report/index.js
@@ -1,23 +1,58 @@
+const fetch = require("node-fetch");
 const { Storage } = require("@google-cloud/storage");
+const { CanvasRenderService } = require("chartjs-node-canvas");
+
 const storage = new Storage();
 
-// [START functions_helloworld_storage]
-/**
- * Generic background Cloud Function to be triggered by Cloud Storage.
- * This sample works for all Cloud Storage CRUD operations.
- *
- * @param {object} file The Cloud Storage file metadata.
- * @param {object} context The event metadata.
- */
+const SLACK_URL =
+  "https://hooks.slack.com/services/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";
+
+async function createChart(game) {
+  const configuration = {
+    type: "line", // for line chart
+    data: {
+      labels: Object.keys(game),
+      datasets: [
+        {
+          label: "Game Eval",
+          data: Object.entries(game),
+          fill: false,
+          borderColor: ["rgba(255, 99, 132, 1)"],
+          borderWidth: 1,
+        },
+      ],
+    },
+  };
+
+  const dataUrl = await canvasRenderService.renderToDataURL(configuration); // converts chart to image
+  return dataUrl;
+}
+
 exports.generateReport = async (file, context) => {
-  console.log(`  Event: ${context.eventId}`);
-  console.log(`  Event Type: ${context.eventType}`);
   console.log(`  Bucket: ${file.bucket}`);
   console.log(`  File: ${file.name}`);
-  console.log(`  Metageneration: ${file.metageneration}`);
-  console.log(`  Created: ${file.timeCreated}`);
-  console.log(`  Updated: ${file.updated}`);
-  const data = await storage.bucket(file.bucket).file(file.name).download(); //stream is created
-  console.log(data[0]);
+  const buffer = await storage.bucket(file.bucket).file(file.name).download();
+  const game = JSON.parse(buffer.toString("utf-9"));
+  new CanvasRenderService(1000, 1000, (chart) => {
+    console.log("chart built");
+    const imgUrl = createChart(game);
+    fetch(SLACK_URL, {
+      method: "POST",
+      body: JSON.stringify({
+        type: "mrkdwn",
+        text: "Chess game",
+        attachments: [
+          {
+            title_link: imgUrl,
+            text: "Your document: <file name>",
+          },
+        ],
+      }),
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    });
+  });
 };
 // [END functions_helloworld_storage]
